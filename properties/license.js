b67eda3cc8500efd388e2b17bfb40445bebf013f
Jack Boberg <hello@jackboberg.info>
2017-05-17 15:59:06 -0400

    refactor: Update var names, use template literal

--- properties/license.js
+++ properties/license.js
@@ -1,21 +1,21 @@
 const validateLicense = require('validate-npm-package-license')
 const { configGetter } = require('../lib')
 
-const validate = (data) => {
-  const its = validateLicense(data)
-
-  if (its.validForNewPackages) return data
-
-  const errors = (its.errors || []).concat(its.warnings || [])
-  const er = new Error('Sorry, ' + errors.join(' and ') + '.')
-  er.notValid = true
-
-  return er
-}
-
 module.exports = ({ config, package: pkg, prompt, yes }) => {
   const conf = configGetter(config)
   const license = pkg.license || conf('init.license') || 'ISC'
 
   return yes ? license : prompt('license', license, validate)
+}
+
+const validate = (input) => {
+  const its = validateLicense(input)
+
+  if (its.validForNewPackages) return input
+
+  const errors = (its.errors || []).concat(its.warnings || [])
+  const er = new Error(`Sorry, ${errors.join(' and ')}.`)
+  er.notValid = true
+
+  return er
 }
