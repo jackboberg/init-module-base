90762c5abf5ca903c950936dad386c7d81c9b2bf
Jack Boberg <hello@jackboberg.info>
2017-05-17 16:53:44 -0400

    refactor: Move functions, extract scoped name

--- properties/name.js
+++ properties/name.js
@@ -2,32 +2,35 @@ const npa = require('npm-package-arg')
 const validateName = require('validate-npm-package-name')
 const { configGetter } = require('../lib')
 
-const niceName = (n) => n.replace(/^node-|[.-]js$/g, '').toLowerCase()
-
-const validate = (data) => {
-  const its = validateName(data)
-
-  if (its.validForNewPackages) return data
-
-  const errors = (its.errors || []).concat(its.warnings || [])
-  const er = new Error('Sorry, ' + errors.join(' and ') + '.')
-  er.notValid = true
-
-  return er
-}
-
 module.exports = ({ basename, config, package: pkg, prompt, yes }) => {
   const conf = configGetter(config)
   let name = pkg.name || basename
   let scope = conf('scope')
 
-  if (scope) {
-    const spec = npa(name)
-
-    if (scope.charAt(0) !== '@') scope = '@' + scope
-    if (spec.scope) name = scope + '/' + spec.name.split('/')[1]
-    else name = scope + '/' + name
-  }
+  if (scope) name = scopedName(name, scope)
 
   return yes ? name : prompt('package name', niceName(name), validate)
+}
+
+const scopedName = (name, scope) => {
+  const spec = npa(name)
+
+  if (spec.scope) name = spec.name.split('/')[1]
+  if (scope.charAt(0) !== '@') scope = `@${scope}`
+
+  return `${scope}/${name}`
+}
+
+const niceName = (name) => name.replace(/^node-|[.-]js$/g, '').toLowerCase()
+
+const validate = (input) => {
+  const its = validateName(input)
+
+  if (its.validForNewPackages) return input
+
+  const errors = (its.errors || []).concat(its.warnings || [])
+  const er = new Error(`Sorry, ${errors.join(' and ')}.`)
+  er.notValid = true
+
+  return er
 }
