3490dcf314110f41d78fc85f1b313f6cfc68c7bd
Jack Boberg <hello@jackboberg.info>
2017-05-17 23:06:02 -0400

    refactor: Adjust variable names

--- properties/version.js
+++ properties/version.js
@@ -1,18 +1,18 @@
 const semver = require('semver')
 const { configGetter } = require('../lib')
 
-const validate = (data) => {
-  if (semver.valid(data)) return data
-
-  const er = new Error('Invalid version: "' + data + '"')
-  er.notValid = true
-
-  return er
-}
-
 module.exports = ({ config, package: pkg, prompt, yes }) => {
   const conf = configGetter(config)
   const version = pkg.version || conf('init.version') || '1.0.0'
 
   return yes ? version : prompt('version', version, validate)
+}
+
+const validate = (input) => {
+  if (semver.valid(input)) return input
+
+  const er = new Error(`Invalid version: "${input}"`)
+  er.notValid = true
+
+  return er
 }
